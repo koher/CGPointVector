901a802732cd368c160804e3c832a8c92101aafb
koher <koher@koherent.org>
2015-12-08 14:11:29 +0900

    Make APIs public

--- CGPointVector/CGPointVector/CGPointVector.swift
+++ CGPointVector/CGPointVector/CGPointVector.swift
@@ -1,135 +1,135 @@
 import CoreGraphics
 
 extension CGPoint {
-	var description: String {
+	public var description: String {
 		return "(\(x), \(y))"
 	}
 	
-	var length: CGFloat {
+	public var length: CGFloat {
 		return sqrt(squareLength)
 	}
 	
-	var squareLength: CGFloat {
+	public var squareLength: CGFloat {
 		return x * x + y * y
 	}
 	
-	var unit: CGPoint {
+	public var unit: CGPoint {
 		return self * (1.0 / length)
 	}
 	
-	var phase: CGFloat {
+	public var phase: CGFloat {
 		return atan2(y, x)
 	}
 	
-	func distanceFrom(point: CGPoint) -> CGFloat {
+	public func distanceFrom(point: CGPoint) -> CGFloat {
 		return (self - point).length
 	}
 	
-	func squareDistanceFrom(point: CGPoint) -> CGFloat {
+	public func squareDistanceFrom(point: CGPoint) -> CGFloat {
 		return (self - point).squareLength
 	}
 	
-	func angleFrom(point: CGPoint) -> CGFloat {
+	public func angleFrom(point: CGPoint) -> CGFloat {
 		return acos(cosOfAngleFrom(point))
 	}
 	
-	func cosOfAngleFrom(point: CGPoint) -> CGFloat {
+	public func cosOfAngleFrom(point: CGPoint) -> CGFloat {
 		return fmin(fmax(self * point / sqrt(self.squareLength * point.squareLength), -1.0), 1.0)
 	}
 	
-	func nearlyEqualTo(point: CGPoint, epsilon: CGFloat) -> Bool {
+	public func nearlyEqualTo(point: CGPoint, epsilon: CGFloat) -> Bool {
 		let difference = self - point
 		return fabs(difference.x) < epsilon && fabs(difference.y) < epsilon
 	}
 }
 
-prefix func + (value: CGPoint) -> CGPoint {
+public prefix func + (value: CGPoint) -> CGPoint {
 	return value
 }
 
-prefix func - (value: CGPoint) -> CGPoint {
+public prefix func - (value: CGPoint) -> CGPoint {
 	return CGPoint(x: -value.x, y: -value.y)
 }
 
-func + (left: CGPoint, right: CGPoint) -> CGPoint {
+public func + (left: CGPoint, right: CGPoint) -> CGPoint {
 	return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
 
-func - (left: CGPoint, right: CGPoint) -> CGPoint {
+public func - (left: CGPoint, right: CGPoint) -> CGPoint {
 	return CGPoint(x: left.x - right.x, y: left.y - right.y)
 }
 
-func * (left: CGPoint, right: CGPoint) -> CGFloat {
+public func * (left: CGPoint, right: CGPoint) -> CGFloat {
 	return left.x * right.x + left.y * right.y
 }
 
-func * (left: CGPoint, right: CGFloat) -> CGPoint {
+public func * (left: CGPoint, right: CGFloat) -> CGPoint {
 	return CGPoint(x: left.x * right, y: left.y * right)
 }
 
-func * (left: CGFloat, right: CGPoint) -> CGPoint {
+public func * (left: CGFloat, right: CGPoint) -> CGPoint {
 	return CGPoint(x: right.x * left, y: right.y * left)
 }
 
-func / (left: CGPoint, right: CGFloat) -> CGPoint {
+public func / (left: CGPoint, right: CGFloat) -> CGPoint {
 	return CGPoint(x: left.x / right, y: left.y / right)
 }
 
-func == (left: CGPoint, right: CGPoint) -> Bool {
+public func == (left: CGPoint, right: CGPoint) -> Bool {
 	return CGPointEqualToPoint(left, right)
 }
 
-func += (inout left: CGPoint, right: CGPoint) {
+public func += (inout left: CGPoint, right: CGPoint) {
 	left = left + right
 }
 
-func -= (inout left: CGPoint, right: CGPoint) {
+public func -= (inout left: CGPoint, right: CGPoint) {
 	left = left - right
 }
 
-func *= (inout left: CGPoint, right: CGFloat) {
+public func *= (inout left: CGPoint, right: CGFloat) {
 	left = left * right
 }
 
-func /= (inout left: CGPoint, right: CGFloat) {
+public func /= (inout left: CGPoint, right: CGFloat) {
 	left = left / right
 }
 
 extension CGAffineTransform {
-	var inverse: CGAffineTransform {
+	public var inverse: CGAffineTransform {
 		return CGAffineTransformInvert(self)
 	}
 	
-	func isIdentity() -> Bool {
+	public func isIdentity() -> Bool {
 		return CGAffineTransformIsIdentity(self)
 	}
 	
-	func nearlyEqualTo(transform: CGAffineTransform, epsilon: CGFloat) -> Bool {
+	public func nearlyEqualTo(transform: CGAffineTransform, epsilon: CGFloat) -> Bool {
 		return fabs(a - transform.a) < epsilon && fabs(b - transform.b) < epsilon && fabs(c - transform.c) < epsilon && fabs(d - transform.d) < epsilon && fabs(tx - transform.tx) < epsilon && fabs(ty - transform.ty) < epsilon
 	}
 }
 
-func * (left: CGAffineTransform, right: CGAffineTransform) -> CGAffineTransform {
+public func * (left: CGAffineTransform, right: CGAffineTransform) -> CGAffineTransform {
 	return CGAffineTransformConcat(left, right)
 }
 
-func * (left: CGAffineTransform, right: CGPoint) -> CGPoint {
+public func * (left: CGAffineTransform, right: CGPoint) -> CGPoint {
 	return CGPointApplyAffineTransform(right, left)
 }
 
-func * (left: CGPoint, right: CGAffineTransform) -> CGPoint {
+public func * (left: CGPoint, right: CGAffineTransform) -> CGPoint {
 	return CGPointApplyAffineTransform(left, right)
 }
 
-func == (left: CGAffineTransform, right: CGAffineTransform) -> Bool {
+public func == (left: CGAffineTransform, right: CGAffineTransform) -> Bool {
 	return CGAffineTransformEqualToTransform(left, right)
 }
 
 extension CGFloat {
-	static var pi: CGFloat {
+	public static var pi: CGFloat {
 		return CGFloat(M_PI)
 	}
-	static var e: CGFloat {
+	public static var e: CGFloat {
 		return CGFloat(M_E)
 	}
 }
