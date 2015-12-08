a570e250ded2281d57e5fe2afe1971a157449fc1
koher <koher@koherent.org>
2015-12-08 14:12:28 +0900

    Replace tabs with spaces

--- CGPointVector/CGPointVector/CGPointVector.swift
+++ CGPointVector/CGPointVector/CGPointVector.swift
@@ -1,135 +1,135 @@
 import CoreGraphics
 
 extension CGPoint {
-	public var description: String {
-		return "(\(x), \(y))"
-	}
-	
-	public var length: CGFloat {
-		return sqrt(squareLength)
-	}
-	
-	public var squareLength: CGFloat {
-		return x * x + y * y
-	}
-	
-	public var unit: CGPoint {
-		return self * (1.0 / length)
-	}
-	
-	public var phase: CGFloat {
-		return atan2(y, x)
-	}
-	
-	public func distanceFrom(point: CGPoint) -> CGFloat {
-		return (self - point).length
-	}
-	
-	public func squareDistanceFrom(point: CGPoint) -> CGFloat {
-		return (self - point).squareLength
-	}
-	
-	public func angleFrom(point: CGPoint) -> CGFloat {
-		return acos(cosOfAngleFrom(point))
-	}
-	
-	public func cosOfAngleFrom(point: CGPoint) -> CGFloat {
-		return fmin(fmax(self * point / sqrt(self.squareLength * point.squareLength), -1.0), 1.0)
-	}
-	
-	public func nearlyEqualTo(point: CGPoint, epsilon: CGFloat) -> Bool {
-		let difference = self - point
-		return fabs(difference.x) < epsilon && fabs(difference.y) < epsilon
-	}
+    public var description: String {
+        return "(\(x), \(y))"
+    }
+    
+    public var length: CGFloat {
+        return sqrt(squareLength)
+    }
+    
+    public var squareLength: CGFloat {
+        return x * x + y * y
+    }
+    
+    public var unit: CGPoint {
+        return self * (1.0 / length)
+    }
+    
+    public var phase: CGFloat {
+        return atan2(y, x)
+    }
+    
+    public func distanceFrom(point: CGPoint) -> CGFloat {
+        return (self - point).length
+    }
+    
+    public func squareDistanceFrom(point: CGPoint) -> CGFloat {
+        return (self - point).squareLength
+    }
+    
+    public func angleFrom(point: CGPoint) -> CGFloat {
+        return acos(cosOfAngleFrom(point))
+    }
+    
+    public func cosOfAngleFrom(point: CGPoint) -> CGFloat {
+        return fmin(fmax(self * point / sqrt(self.squareLength * point.squareLength), -1.0), 1.0)
+    }
+    
+    public func nearlyEqualTo(point: CGPoint, epsilon: CGFloat) -> Bool {
+        let difference = self - point
+        return fabs(difference.x) < epsilon && fabs(difference.y) < epsilon
+    }
 }
 
 public prefix func + (value: CGPoint) -> CGPoint {
-	return value
+    return value
 }
 
 public prefix func - (value: CGPoint) -> CGPoint {
-	return CGPoint(x: -value.x, y: -value.y)
+    return CGPoint(x: -value.x, y: -value.y)
 }
 
 public func + (left: CGPoint, right: CGPoint) -> CGPoint {
-	return CGPoint(x: left.x + right.x, y: left.y + right.y)
+    return CGPoint(x: left.x + right.x, y: left.y + right.y)
 }
 
 public func - (left: CGPoint, right: CGPoint) -> CGPoint {
-	return CGPoint(x: left.x - right.x, y: left.y - right.y)
+    return CGPoint(x: left.x - right.x, y: left.y - right.y)
 }
 
 public func * (left: CGPoint, right: CGPoint) -> CGFloat {
-	return left.x * right.x + left.y * right.y
+    return left.x * right.x + left.y * right.y
 }
 
 public func * (left: CGPoint, right: CGFloat) -> CGPoint {
-	return CGPoint(x: left.x * right, y: left.y * right)
+    return CGPoint(x: left.x * right, y: left.y * right)
 }
 
 public func * (left: CGFloat, right: CGPoint) -> CGPoint {
-	return CGPoint(x: right.x * left, y: right.y * left)
+    return CGPoint(x: right.x * left, y: right.y * left)
 }
 
 public func / (left: CGPoint, right: CGFloat) -> CGPoint {
-	return CGPoint(x: left.x / right, y: left.y / right)
+    return CGPoint(x: left.x / right, y: left.y / right)
 }
 
 public func == (left: CGPoint, right: CGPoint) -> Bool {
-	return CGPointEqualToPoint(left, right)
+    return CGPointEqualToPoint(left, right)
 }
 
 public func += (inout left: CGPoint, right: CGPoint) {
-	left = left + right
+    left = left + right
 }
 
 public func -= (inout left: CGPoint, right: CGPoint) {
-	left = left - right
+    left = left - right
 }
 
 public func *= (inout left: CGPoint, right: CGFloat) {
-	left = left * right
+    left = left * right
 }
 
 public func /= (inout left: CGPoint, right: CGFloat) {
-	left = left / right
+    left = left / right
 }
 
 extension CGAffineTransform {
-	public var inverse: CGAffineTransform {
-		return CGAffineTransformInvert(self)
-	}
-	
-	public func isIdentity() -> Bool {
-		return CGAffineTransformIsIdentity(self)
-	}
-	
-	public func nearlyEqualTo(transform: CGAffineTransform, epsilon: CGFloat) -> Bool {
-		return fabs(a - transform.a) < epsilon && fabs(b - transform.b) < epsilon && fabs(c - transform.c) < epsilon && fabs(d - transform.d) < epsilon && fabs(tx - transform.tx) < epsilon && fabs(ty - transform.ty) < epsilon
-	}
+    public var inverse: CGAffineTransform {
+        return CGAffineTransformInvert(self)
+    }
+    
+    public func isIdentity() -> Bool {
+        return CGAffineTransformIsIdentity(self)
+    }
+    
+    public func nearlyEqualTo(transform: CGAffineTransform, epsilon: CGFloat) -> Bool {
+        return fabs(a - transform.a) < epsilon && fabs(b - transform.b) < epsilon && fabs(c - transform.c) < epsilon && fabs(d - transform.d) < epsilon && fabs(tx - transform.tx) < epsilon && fabs(ty - transform.ty) < epsilon
+    }
 }
 
 public func * (left: CGAffineTransform, right: CGAffineTransform) -> CGAffineTransform {
-	return CGAffineTransformConcat(left, right)
+    return CGAffineTransformConcat(left, right)
 }
 
 public func * (left: CGAffineTransform, right: CGPoint) -> CGPoint {
-	return CGPointApplyAffineTransform(right, left)
+    return CGPointApplyAffineTransform(right, left)
 }
 
 public func * (left: CGPoint, right: CGAffineTransform) -> CGPoint {
-	return CGPointApplyAffineTransform(left, right)
+    return CGPointApplyAffineTransform(left, right)
 }
 
 public func == (left: CGAffineTransform, right: CGAffineTransform) -> Bool {
-	return CGAffineTransformEqualToTransform(left, right)
+    return CGAffineTransformEqualToTransform(left, right)
 }
 
 extension CGFloat {
-	public static var pi: CGFloat {
-		return CGFloat(M_PI)
-	}
-	public static var e: CGFloat {
-		return CGFloat(M_E)
-	}
+    public static var pi: CGFloat {
+        return CGFloat(M_PI)
+    }
+    public static var e: CGFloat {
+        return CGFloat(M_E)
+    }
 }
--- CGPointVector/CGPointVectorTests/CGPointVectorTests.swift
+++ CGPointVector/CGPointVectorTests/CGPointVectorTests.swift
@@ -2,84 +2,84 @@ import XCTest
 import CGPointVector
 
 class CGPointVectorTests: XCTestCase {
-	let torelance: CGFloat = 1.0e-5;
-	
-	override func setUp() {
-		super.setUp()
-	}
-	
-	override func tearDown() {
-		super.tearDown()
-	}
-	
-	func testNearlyEqualTo() {
-		XCTAssert(CGPoint(x: 1.0, y: 2.0).nearlyEqualTo(CGPoint(x: 1.0, y: 2.9), epsilon: 1.0))
-		XCTAssert(!CGPoint(x: 1.0, y: 2.0).nearlyEqualTo(CGPoint(x: 1.0, y: 3.0), epsilon: 1.0))
-		XCTAssert(CGPoint(x: 1.0, y: 2.0).nearlyEqualTo(CGPoint(x: 0.51, y: 2.0), epsilon: 0.5))
-		XCTAssert(!CGPoint(x: 1.0, y: 2.0).nearlyEqualTo(CGPoint(x: 0.5, y: 2.0), epsilon: 0.5))
-	}
-	
-	func testDescription() {
-		XCTAssertEqual(CGPoint(x: 1.0, y: 2.0).description, "(1.0, 2.0)")
-	}
-	
-	func testAdd() {
-		XCTAssert((CGPoint(x: 1.0, y: 2.0) + CGPoint(x: 3.0, y: -4.0)).nearlyEqualTo(CGPoint(x: 4.0, y: -2.0), epsilon: torelance))
-	}
-	
-	func testSubtract() {
-		XCTAssert((CGPoint(x: 3.0, y: 2.0) - CGPoint(x: 1.0, y: 4.0)).nearlyEqualTo(CGPoint(x: 2.0, y: -2.0), epsilon: torelance))
-	}
-	
-	func testMultiply() {
-		XCTAssert((CGPoint(x: 1.0, y: -2.0) * 3.0).nearlyEqualTo(CGPoint(x: 3.0, y: -6.0), epsilon: torelance))
-		XCTAssert((3.0 * CGPoint(x: 1.0, y: -2.0)).nearlyEqualTo(CGPoint(x: 3.0, y: -6.0), epsilon: torelance))
-		
-		XCTAssert((CGPoint(x: 1.0, y: 1.0) * CGAffineTransformMakeRotation(CGFloat.pi / 4)).nearlyEqualTo(CGPoint(x: 0.0, y: sqrt(2)), epsilon: torelance))
-		XCTAssert((CGAffineTransformMakeTranslation(1.0, -2.0) * CGPoint(x: 1.0, y: 1.0)).nearlyEqualTo(CGPoint(x: 2.0, y: -1.0), epsilon: torelance))
-	}
-	
-	func testDivide() {
-		XCTAssert((CGPoint(x: 8.0, y: -2.0) / 4.0).nearlyEqualTo(CGPoint(x: 2.0, y: -0.5), epsilon: torelance))
-	}
-	
-	func testNegate() {
-		XCTAssert((-CGPoint(x: 1.0, y: -2.0)).nearlyEqualTo(CGPoint(x: -1.0, y: 2.0), epsilon: torelance))
-	}
-	
-	func testUnit() {
-		XCTAssert(CGPoint(x: 3.0, y: -4.0).unit.nearlyEqualTo(CGPoint(x: 0.6, y: -0.8), epsilon: torelance))
-	}
-	
-	func testLength() {
-		XCTAssertEqualWithAccuracy(CGPoint(x: 3.0, y: -4.0).length, 5.0, accuracy: torelance)
-	}
-	
-	func testSquareLength() {
-		XCTAssertEqualWithAccuracy(CGPoint(x: 3.0, y: -4.0).squareLength, 25.0, accuracy: torelance)
-	}
-	
-	func testPhase() {
-		XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: sqrt(3)).phase, CGFloat.pi / 3, accuracy: torelance)
-	}
-	
-	func testDistance() {
-		XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 2.0).distanceFrom(CGPoint(x: -3.0, y: 5.0)), 5.0, accuracy: torelance)
-	}
-	
-	func testSquareDistance() {
-		XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 2.0).squareDistanceFrom(CGPoint(x: -3.0, y: 5.0)), 25.0, accuracy: torelance)
-	}
-	
-	func testDotProduct() {
-		XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 2.0) * CGPoint(x: -3.0, y: 4.0), 5.0, accuracy: torelance)
-	}
-	
-	func testAngleFrom() {
-		XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 0.0).angleFrom(CGPoint(x: sqrt(3.0), y: 1.0)), CGFloat.pi / 6, accuracy: torelance)
-	}
-	
-	func testCosOfAngleFrom() {
-		XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 0.0).cosOfAngleFrom(CGPoint(x: 1.0, y: sqrt(3.0))), 0.5, accuracy: torelance)
-	}
+    let torelance: CGFloat = 1.0e-5;
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testNearlyEqualTo() {
+        XCTAssert(CGPoint(x: 1.0, y: 2.0).nearlyEqualTo(CGPoint(x: 1.0, y: 2.9), epsilon: 1.0))
+        XCTAssert(!CGPoint(x: 1.0, y: 2.0).nearlyEqualTo(CGPoint(x: 1.0, y: 3.0), epsilon: 1.0))
+        XCTAssert(CGPoint(x: 1.0, y: 2.0).nearlyEqualTo(CGPoint(x: 0.51, y: 2.0), epsilon: 0.5))
+        XCTAssert(!CGPoint(x: 1.0, y: 2.0).nearlyEqualTo(CGPoint(x: 0.5, y: 2.0), epsilon: 0.5))
+    }
+    
+    func testDescription() {
+        XCTAssertEqual(CGPoint(x: 1.0, y: 2.0).description, "(1.0, 2.0)")
+    }
+    
+    func testAdd() {
+        XCTAssert((CGPoint(x: 1.0, y: 2.0) + CGPoint(x: 3.0, y: -4.0)).nearlyEqualTo(CGPoint(x: 4.0, y: -2.0), epsilon: torelance))
+    }
+    
+    func testSubtract() {
+        XCTAssert((CGPoint(x: 3.0, y: 2.0) - CGPoint(x: 1.0, y: 4.0)).nearlyEqualTo(CGPoint(x: 2.0, y: -2.0), epsilon: torelance))
+    }
+    
+    func testMultiply() {
+        XCTAssert((CGPoint(x: 1.0, y: -2.0) * 3.0).nearlyEqualTo(CGPoint(x: 3.0, y: -6.0), epsilon: torelance))
+        XCTAssert((3.0 * CGPoint(x: 1.0, y: -2.0)).nearlyEqualTo(CGPoint(x: 3.0, y: -6.0), epsilon: torelance))
+        
+        XCTAssert((CGPoint(x: 1.0, y: 1.0) * CGAffineTransformMakeRotation(CGFloat.pi / 4)).nearlyEqualTo(CGPoint(x: 0.0, y: sqrt(2)), epsilon: torelance))
+        XCTAssert((CGAffineTransformMakeTranslation(1.0, -2.0) * CGPoint(x: 1.0, y: 1.0)).nearlyEqualTo(CGPoint(x: 2.0, y: -1.0), epsilon: torelance))
+    }
+    
+    func testDivide() {
+        XCTAssert((CGPoint(x: 8.0, y: -2.0) / 4.0).nearlyEqualTo(CGPoint(x: 2.0, y: -0.5), epsilon: torelance))
+    }
+    
+    func testNegate() {
+        XCTAssert((-CGPoint(x: 1.0, y: -2.0)).nearlyEqualTo(CGPoint(x: -1.0, y: 2.0), epsilon: torelance))
+    }
+    
+    func testUnit() {
+        XCTAssert(CGPoint(x: 3.0, y: -4.0).unit.nearlyEqualTo(CGPoint(x: 0.6, y: -0.8), epsilon: torelance))
+    }
+    
+    func testLength() {
+        XCTAssertEqualWithAccuracy(CGPoint(x: 3.0, y: -4.0).length, 5.0, accuracy: torelance)
+    }
+    
+    func testSquareLength() {
+        XCTAssertEqualWithAccuracy(CGPoint(x: 3.0, y: -4.0).squareLength, 25.0, accuracy: torelance)
+    }
+    
+    func testPhase() {
+        XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: sqrt(3)).phase, CGFloat.pi / 3, accuracy: torelance)
+    }
+    
+    func testDistance() {
+        XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 2.0).distanceFrom(CGPoint(x: -3.0, y: 5.0)), 5.0, accuracy: torelance)
+    }
+    
+    func testSquareDistance() {
+        XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 2.0).squareDistanceFrom(CGPoint(x: -3.0, y: 5.0)), 25.0, accuracy: torelance)
+    }
+    
+    func testDotProduct() {
+        XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 2.0) * CGPoint(x: -3.0, y: 4.0), 5.0, accuracy: torelance)
+    }
+    
+    func testAngleFrom() {
+        XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 0.0).angleFrom(CGPoint(x: sqrt(3.0), y: 1.0)), CGFloat.pi / 6, accuracy: torelance)
+    }
+    
+    func testCosOfAngleFrom() {
+        XCTAssertEqualWithAccuracy(CGPoint(x: 1.0, y: 0.0).cosOfAngleFrom(CGPoint(x: 1.0, y: sqrt(3.0))), 0.5, accuracy: torelance)
+    }
 }
