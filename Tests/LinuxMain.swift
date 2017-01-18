44cbf578b5a6af8d31f08ebb84702171adf70548
Yuta Koshizawa <koher@koherent.org>
2017-01-18 20:27:29 +0900

    Update LinuxMain.swift

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,5 +2,8 @@ import XCTest
 @testable import CGPointVectorTests
 
 XCTMain([
-     testCase(CGPointVectorTests.allTests),
+     testCase(CGAffineTransormTests.allTests),
+     testCase(CGPointTests.allTests),
+     testCase(CGRectTests.allTests),
+     testCase(CGSizeTests.allTests),
 ])
